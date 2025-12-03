258162b77ed0a2ea98534c132a0d9a09ef6664ef
Artem P <artmp24@yandex.ru>
2025-12-04 01:56:33 +0300

    fix project structure

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("se.patrikerdes.use-latest-versions") version "0.2.19"
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "7.0.1.6134"
+    application
+    checkstyle
+}
+
+application { mainClass.set("hexlet.code.App") }
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
