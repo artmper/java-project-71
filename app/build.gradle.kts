736a7076e4f5dd97a8aeb272b32e35d6eeadf74f
Artem P <artmp24@yandex.ru>
2025-12-07 21:20:15 +0300

    update Makefile & build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
     id("com.github.ben-manes.versions") version "0.53.0"
     id("org.sonarqube") version "7.0.1.6134"
-    alias(libs.plugins.spotless)
-    alias(libs.plugins.lombok)
-    alias(libs.plugins.shadow)
-    alias(libs.plugins.sonarqube)
     application
     checkstyle
     jacoco
@@ -34,57 +30,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
 }
 
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use JUnit Jupiter test framework
-            useJUnitJupiter(libs.versions.junit.get())
-        }
-    }
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
-
-tasks.test {
-    testLogging {
-        showStandardStreams = true
-
-        // какие события показывать
-        events(
-            TestLogEvent.FAILED,
-            TestLogEvent.PASSED,
-            TestLogEvent.SKIPPED,
-            TestLogEvent.STANDARD_OUT,
-            TestLogEvent.STANDARD_ERROR,
-        )
-
-        // формат исключений
-        exceptionFormat = TestExceptionFormat.FULL
-
-        // детали
-        showExceptions = true
-        showCauses = true
-        showStackTraces = true
-    }
-}
-
-spotless {
-    java {
-        // don't need to set target, it is inferred from java
-
-        // apply a specific flavor of google-java-format
-        // googleJavaFormat('1.8').aosp().reflowLongStrings().skipJavadocFormatting()
-        // fix formatting of type annotations
-        importOrder()
-        googleJavaFormat().aosp()
-        formatAnnotations()
-        removeUnusedImports()
-        leadingTabsToSpaces(4)
-        endWithNewline()
-        // make sure every file has the following copyright header.
-        // optionally, Spotless can set copyright years by digging
-        // through git history (see "license" section below)
-        // licenseHeader '/* (C)$YEAR */'
-    }
-}
-
 tasks.jacocoTestReport { reports { xml.required.set(true) } }
